ac237577cc06971bd74f20f965bd084ec6f55125
Ivan Martin <martinivan120@gmail.com>
2022-07-01 12:54:01 +0200

    added experience section

--- src/app/dashboard/views/experience/experience.component.ts
+++ src/app/dashboard/views/experience/experience.component.ts
@@ -7,9 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+  listItemSelected: string = 'deimos';
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onClickListEvent(event: any) {
+    const id = event.srcElement.id;
+    const element = event.target;
+
+    switch (id) {
+      case 'accenture':
+        if (element.classList.contains('list-item-clicked')) {
+          /* event.target.classList.remove('list-item-clicked');
+          this.listItemSelected = ''; */
+        } else {
+          event.target.classList.add('list-item-clicked');
+          //remove all classes from other elements
+          document.getElementById('deimos')?.classList.remove('list-item-clicked');
+          this.listItemSelected = id;
+        }
+        break;
+      case 'deimos':
+        if (element.classList.contains('list-item-clicked')) {
+          /* event.target.classList.remove('list-item-clicked');
+          this.listItemSelected = ''; */
+        } else {
+          event.target.classList.add('list-item-clicked');
+          //remove all classes from other elements
+          document.getElementById('accenture')?.classList.remove('list-item-clicked');
+          this.listItemSelected = id;        }
+        break;
+      default:
+        break;
+    }
+
+
   }
 
 }
